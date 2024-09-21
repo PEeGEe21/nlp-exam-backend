71bd2e6de977948d45f6a9d111867d56ac4b3dc9
praise <mailpraiseudeh@gmail.com>
2024-09-21 15:05:32 +0100

    added entities to tests module

--- src/tests/tests.module.ts
+++ src/tests/tests.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TestsController } from './controllers/tests.controller';
 import { TestsService } from './services/tests.service';
+import { UsersModule } from 'src/users/users.module';
+import { DifficultyTypesModule } from 'src/difficulty-types/difficulty-types.module';
+import { OptionTypesModule } from 'src/option-types/option-types.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/typeorm/entities/User';
+import { DifficultyType } from 'src/typeorm/entities/DifficultyType';
+import { Question } from 'src/typeorm/entities/Question';
+import { Answer } from 'src/typeorm/entities/Answer';
+import { OptionType } from 'src/typeorm/entities/OptionType';
 
 @Module({
+  imports: [
+    UsersModule,
+    DifficultyTypesModule,
+    OptionTypesModule,
+    TypeOrmModule.forFeature([User, DifficultyType, Question, Answer, OptionType])
+  ],
   controllers: [TestsController],
-  providers: [TestsService]
+  providers: [TestsService],
+  exports: [TestsService]
 })
 export class TestsModule {}
