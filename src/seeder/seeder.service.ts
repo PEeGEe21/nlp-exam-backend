bf554344cefb4aea235af29b3f8f47fa44f9eaa5
praise <mailpraiseudeh@gmail.com>
2025-01-18 18:49:09 +0100

    fix seeder

--- src/seeder/seeder.service.ts
+++ src/seeder/seeder.service.ts
@@ -80,11 +80,11 @@ export class SeederService {
           id: 1, 
           title: "Multiple Choice Single Answer"
       },
-      {
-          description: "An option type where explanations are required as answer.", 
-          id: 2, 
-          title: "Subjective Question"
-      },
+      // {
+      //     description: "An option type where explanations are required as answer.", 
+      //     id: 2, 
+      //     title: "Subjective Question"
+      // },
       {
           description: "An option type where one can give their own answer", 
           id: 3, 
